7510c81403b4a143fff8faed511906a09bd1272c
YADAV <YADAV@DESKTOP-K99VVV3>
2026-01-23 01:04:25 +0530

    Updated the Pom.xml & added Project Lombok Usage to Our App.

--- Todo-App-SpringBoot-Thymleaf/src/main/java/com/code/entity/Todo.java
+++ Todo-App-SpringBoot-Thymleaf/src/main/java/com/code/entity/Todo.java
@@ -1,29 +1,13 @@
 package com.code.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Todo {
-	
+
 	private String task;
 	private boolean done;
-	public String getTask() {
-		return task;
-	}
-	public void setTask(String task) {
-		this.task = task;
-	}
-	public boolean isDone() {
-		return done;
-	}
-	public void setDone(Boolean done) {
-		this.done = done;
-	}
-	public Todo(String task, Boolean done) {
-		super();
-		this.task = task;
-		this.done = done;
-	}
-	public Todo() {
-		super();
-	}
-	
 
 }
